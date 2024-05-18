9e771af599091c67de1087c11ad5b5b00587e358
Mandoiu Vlad - Stefan <vlad.mandoiu@student.unitbv.ro>
2024-05-18 16:58:52 +0300

    implement delete from grid functionality for user

--- tema3/tema3/Commands/RelayCommand.cs
+++ tema3/tema3/Commands/RelayCommand.cs
@@ -1,35 +1,81 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 
-namespace Supermarket.ViewModels.Commands;
-
-public class RelayCommand<T>(Action<T> workToDo, Predicate<T> canExecute) : ICommand
+namespace Supermarket.ViewModels.Commands
 {
-    private readonly Action<T> _commandTask = workToDo;
-
-    public RelayCommand(Action<T> workToDo)
-        : this(workToDo, DefaultCanExecute)
+    public class RelayCommand<T> : ICommand
     {
-        _commandTask = workToDo;
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? DefaultCanExecute;
+        }
+
+        private static bool DefaultCanExecute(T parameter)
+        {
+            return true;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
     }
 
-    private static bool DefaultCanExecute(T parameter)
+    public class RelayCommand : ICommand
     {
-        return true;
-    }
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
 
-    public bool CanExecute(object parameter)
-    {
-        return canExecute != null && canExecute((T)parameter);
-    }
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? DefaultCanExecute;
+        }
 
-    public event EventHandler CanExecuteChanged
-    {
-        add => CommandManager.RequerySuggested += value;
-        remove => CommandManager.RequerySuggested -= value;
-    }
+        private static bool DefaultCanExecute()
+        {
+            return true;
+        }
 
-    public void Execute(object parameter)
-    {
-        _commandTask((T)parameter);
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
     }
 }
--- tema3/tema3/ViewModels/AdminViewModel.cs
+++ tema3/tema3/ViewModels/AdminViewModel.cs
@@ -1,31 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Supermarket.ViewModels.Commands;
 using tema3.Models.BusinessLogicLayer;
+using tema3.Models.DataAccessLayer;
 using tema3.Models.Entities;
-using User = Microsoft.VisualBasic.ApplicationServices.User;
 
 namespace tema3.ViewModels
 {
     public class AdminViewModel : BaseViewModel
     {
-        public ObservableCollection<tema3.Models.Entities.User> Users { get; set; }   
+        public ObservableCollection<User> Users { get; set; }
         public ObservableCollection<Product> Products { get; set; }
         public ObservableCollection<Category> Categories { get; set; }
         public ObservableCollection<Producer> Producers { get; set; }
         public ObservableCollection<Receipt> Receipts { get; set; }
         public ObservableCollection<Stock> Stocks { get; set; }
 
-        private UserBLL userBLL = new UserBLL();
-        private ProductBLL productBLL = new ProductBLL();
-        private CategoryBLL categoryBLL = new CategoryBLL();
-        private ProducerBLL producerBLL = new ProducerBLL();
-        private ReceiptBLL receiptBLL = new ReceiptBLL();
-        private StockBLL stockBLL = new StockBLL();
-        
+        private readonly UserBLL userBLL = new UserBLL();
+        private readonly ProductBLL productBLL = new ProductBLL();
+        private readonly CategoryBLL categoryBLL = new CategoryBLL();
+        private readonly ProducerBLL producerBLL = new ProducerBLL();
+        private readonly ReceiptBLL receiptBLL = new ReceiptBLL();
+        private readonly StockBLL stockBLL = new StockBLL();
+        private readonly UserDAL userDAL = new UserDAL();
+
+        public ICommand DeleteUserCommand { get; set; }
+
+        private User _selectedUser;
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                if (_selectedUser != value)
+                {
+                    _selectedUser = value;
+                    OnPropertyChanged(nameof(SelectedUser));
+                }
+            }
+        }
+
         public AdminViewModel()
         {
             Users = userBLL.GetAllUsers();
@@ -34,8 +48,18 @@ namespace tema3.ViewModels
             Producers = producerBLL.GetAllProducers();
             Receipts = receiptBLL.GetAllReceipts();
             Stocks = stockBLL.GetAllStocks();
+
+            DeleteUserCommand = new RelayCommand(DeleteUser);
+        }
+
+        private void DeleteUser()
+        {
+            if (SelectedUser.IsActive)
+            {
+                userDAL.DeleteUser(SelectedUser.UserId);
+                Users = userBLL.GetAllUsers();
+                OnPropertyChanged(nameof(Users));
+            }
         }
     }
-
-    
 }
